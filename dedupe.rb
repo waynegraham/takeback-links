92aa2341284e0949b7a56352f1a54078ce7f895a
Wayne Graham <wayne.graham@virginia.edu>
2015-01-13 16:52:05 -0500

    dedupe on the entire dataset

--- dedupe.rb
+++ dedupe.rb
@@ -1,9 +1,9 @@
 require 'csv'
 require 'pp'
 
-csv = CSV.read('links/links.csv').to_a
-
-csv.uniq.each do |link|
-  pp link[0] + ","
+csv = CSV.read('links/links.csv').to_a.uniq
+pp "url,notes"
+csv.each do |link|
+  pp link[0]
 end
 
